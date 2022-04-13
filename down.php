02cf17027d23cfcbdeb159cab3c77334b0809530
ronnaldaraujo <r17.ronald17@gmail.com>
2022-04-12 22:47:15 -0300

    first commit

---
+++ down.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST['button'])) {
+    $imgUrl = $_POST['imgurl'];
+    $ch = curl_init($imgUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $downloadImg = curl_exec($ch);
+    curl_close($ch);
+    header('Content-type: image/jpg');
+    header('Content-Disposition: attachment;filename="thumbnail.jpg"');
+    echo $downloadImg;
+}
